fa0ef462fbee261d5ec2ab7419e0ce05e02228d1
Alpha <rohithss2001@gmail.com>
2023-04-20 15:56:26 +0530

    Login page code

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-
 document.getElementById("log_icon").src="assests/flickr (1).png";
 document.getElementById("log_icon").style="height:4rem"
 document.getElementById("log_img").style.cssText="margin-left: auto;margin-right: auto;height: 2rem; width: 2rem;"
 document.getElementById("log_img").src="assests/flickr (1).png";
-document.getElementById("log_box").style.cssText="right: 0px;width:25%;background-color:white;margin-top:auto;margin-bottom:auto;position:relative;border-radius:0.5rem;display:flex;flex-direction: column;padding:20px"
+document.getElementById("log_box").style.cssText="right: 0px;width:100%;background-color:white;margin-top:auto;margin-bottom:auto;position:relative;border-radius:0.125rem;display:flex;flex-direction: column;padding:20px"
 document.getElementById("section1").style.cssText="text-align: center;"
 document.getElementById("h1").style.cssText="font-weight:bold"
