f9e5898831b1a81f2a1dc9e49e25b84bd0a4379b
Alpha <rohithss2001@gmail.com>
2023-04-20 08:34:07 +0530

    Code : Modified

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-document.getElementById("header").style.cssText="position: fixed;width: 100%;z-index: 1;background-color: #f50018";
+
+document.getElementById("log_icon").src="assests/flickr (1).png";
+document.getElementById("log_icon").style="height:4rem"
+document.getElementById("log_img").style.cssText="margin-left: auto;margin-right: auto;height: 2rem; width: 2rem;"
+document.getElementById("log_img").src="assests/flickr (1).png";
+document.getElementById("log_box").style.cssText="right: 0px;width:25%;background-color:white;margin-top:auto;margin-bottom:auto;position:relative;border-radius:0.5rem;display:flex;flex-direction: column;padding:20px"
+document.getElementById("section1").style.cssText="text-align: center;"
+document.getElementById("h1").style.cssText="font-weight:bold"
