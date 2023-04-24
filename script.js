3495662d7233919904fb418e4f6b1adc3d0473a3
Alpha <rohithss2001@gmail.com>
2023-04-24 16:55:29 +0530

    Background Ready

--- script.js
+++ script.js
@@ -5,3 +5,6 @@ document.getElementById("log_img").src="assests/flickr (1).png";
 document.getElementById("log_box").style.cssText="right: 0px;width:100%;background-color:white;margin-top:auto;margin-bottom:auto;position:relative;border-radius:0.125rem;display:flex;flex-direction: column;padding:20px"
 document.getElementById("section1").style.cssText="text-align: center;"
 document.getElementById("h1").style.cssText="font-weight:bold"
+
+
+
